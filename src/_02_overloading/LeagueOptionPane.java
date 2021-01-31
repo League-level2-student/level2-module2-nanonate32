e49a8d877cb73a0669e2a63d694fdc49dfc2bd72
natha <natha@nathan-gaming-rig.lan>
2021-01-30 17:57:32 -0800

    LeagueOptionPane PepperoniPizza

--- src/_02_overloading/LeagueOptionPane.java
+++ src/_02_overloading/LeagueOptionPane.java
@@ -20,14 +20,18 @@ public class LeagueOptionPane {
 	public static void showMessageDialog(String message) {
 		// 1. Open example.png and make a GUI that looks like that
 		//    The message parameter is what we want to show on our pop-up
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
 		
+		frame.setVisible(true);
+		 
 		
 		// 2. Uncomment the line of code below. It sets the location of our frame to the center of the screen
-		//frame.setLocationRelativeTo(null);
+		frame.setLocationRelativeTo(null);
 	}
 	
 	// 3. Call this method in the Runner class
-	
+	     
 
 	// 4. Create another showMessageDialog() method that lets us also choose the Message and Title 
 	//    2 String parameters (one for the message and one for the title)
--- src/_02_overloading/LeagueOptionPaneRunner.java
+++ src/_02_overloading/LeagueOptionPaneRunner.java
@@ -2,6 +2,6 @@ package _02_overloading;
 
 public class LeagueOptionPaneRunner {
 	public static void main(String[] args) {
-		
+		showMessageDialog("The League is the Best");
 	}
 }
