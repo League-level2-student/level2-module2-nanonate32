0a9d2f173d2d4c3dea0b18af51dafe8a727d3fb5
natha <natha@nathan-gaming-rig.lan>
2021-05-29 17:52:42 -0700

    FlappyMario

---
+++ src/FlappyMario.java
@@ -0,0 +1,42 @@
+
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class FlappyMario extends JFrame {
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	
+	public void run() {
+		BufferedImage player = null;
+		try {
+			player = ImageIO.read(this.getClass().getResourceAsStream("MarioKart.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		label.setIcon(new ImageIcon(player));
+		BufferedImage pipe = null;
+		try {
+			player = ImageIO.read(this.getClass().getResourceAsStream("pipe.jpeg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		label.setIcon(new ImageIcon(pipe));
+
+	frame.add(panel);	
+	panel.add(label);
+	frame.setVisible(true);
+	frame.pack();
+	
+	}
+}
--- src/PongRunner.java
+++ src/PongRunner.java
@@ -1,11 +1,12 @@
-package intro_to_array_lists;
+
 
 public class PongRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      Pong pong = new Pong();
-      pong.run();
+      FlappyMario mario = new FlappyMario();
+     mario.run();
+      
 	}
 
 }
